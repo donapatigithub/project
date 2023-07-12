9779dd31720c9065d611155e84af23e637b1fd1a
Donapati <donapatisivareddy8@gmail.com>
2023-07-12 15:11:22 +0530

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import com.example.project1.R
 
 class MainActivity : AppCompatActivity() {
+    //checking git
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
