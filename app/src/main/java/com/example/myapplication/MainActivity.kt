5f53899866e946c0b1e7fba390d783f6e0ad68b6
Donapati <donapatisivareddy8@gmail.com>
2023-07-17 10:47:46 +0530

    commit

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,6 +2,10 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil.setContentView
 import com.example.project1.R
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val welcome : TextView = findViewById(R.id.welcome)
+        val editText : EditText = findViewById(R.id.edit)
+        val button : Button = findViewById(R.id.btn)
+        val textView : TextView = findViewById(R.id.txtview)
     }
 }
