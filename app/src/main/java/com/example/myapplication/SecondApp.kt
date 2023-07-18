197b79be534d74e1ce16ee72787d52db96d37b21
Donapati <donapatisivareddy8@gmail.com>
2023-07-18 12:58:20 +0530

    commit

--- app/src/main/java/com/example/myapplication/SecondApp.kt
+++ app/src/main/java/com/example/myapplication/SecondApp.kt
@@ -1,10 +1,8 @@
 package com.example.myapplication
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.TextView
 import com.example.project1.R
 
 class SecondApp : AppCompatActivity() {
